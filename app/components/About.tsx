bcb5dd8e0444574209c2adac856b0834e76072ef
Manthan Khawse <86763361+manthankhawse@users.noreply.github.com>
2024-12-29 13:36:29 +0530

    Update About.tsx

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -5,7 +5,7 @@ function About() {
     <>
         <div className="flex-col w-full py-16">
             <h1 className="text-3xl font-bold text-center mb-12">About Us</h1>
-            <p className='text-center w-1/2 mx-auto text-xl'>At New-Gen, we specialize in helping businesses in the chemical industry optimize processes, ensure compliance, and drive sustainable growth. With a fresh perspective and a results-driven approach, we deliver tailored solutions that simplify complexities and enhance efficiency. Partner with us to navigate challenges and achieve your goals.</p>
+            <p className='text-center w-1/2 mx-auto text-xl'>At New-Gen Solutions, we specialize in helping businesses in the chemical industry optimize processes, ensure compliance, and drive sustainable growth. With a fresh perspective and a results-driven approach, we deliver tailored solutions that simplify complexities and enhance efficiency. Partner with us to navigate challenges and achieve your goals.</p>
         </div>
     </>
   )
