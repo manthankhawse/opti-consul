1690e96c7ca495e7bf3c046ad2f680755dcc7915
Manthan Khawse <86763361+manthankhawse@users.noreply.github.com>
2024-12-29 13:26:05 +0530

    Update Navbar.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import { NavigationMenuDemo } from "./NavigationMenu"
 const Navbar = () => {
   return (
     <div className="flex justify-between items-center px-12 py-4 border-b-2">
-        <div className="font-bold text-2xl">Logo</div>
+        <div className="font-bold text-2xl">New-Gen</div>
         <NavigationMenuDemo/>
         <Button>Get a demo</Button>
     </div>
